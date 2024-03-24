0b1ff6e0f3bc9bc7da91f39ce0709e4cdec1bd18
Ender <ndxendernight@gmail.com>
2024-03-25 00:51:10 +0100

    Added file reader and changed clang-format

--- main.cc
+++ main.cc
@@ -1,8 +1,16 @@
+#include <fstream>
 #include <iostream>
 
 int main(int argc, char *argv[])
 {
-  std::cout << "Hello World!" << std::endl;
+    if (argc >= 2) {
+        std::ifstream file(argv[1]);
 
-  return 0;
+        if (!file.is_open())
+            std::cerr << "File not found" << std::endl;
+        else
+            std::cout << file.rdbuf() << std::endl;
+    }
+
+    return 0;
 }
